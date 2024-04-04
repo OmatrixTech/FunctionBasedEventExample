4caf5e570d0c130402b7c951fcb91e182f9e3fca
OmatrixTech <144843226+OmatrixTech@users.noreply.github.com>
2024-04-04 21:44:30 +0530

    Add files via upload

---
+++ FunctionBasedEventExample/EventHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionBasedEventExample
+{
+    public class EventHandler
+    {
+        // Declare an event of type Func<Task>
+        public event Func<Task> FunctionEvent;
+
+        // Method to raise the event
+        public async Task TriggerCollectionChanged()
+        {
+            Func<Task> pFeedSourceEcosystemsCollectionChanged = FunctionEvent;
+            if (pFeedSourceEcosystemsCollectionChanged != null)
+            {
+                await pFeedSourceEcosystemsCollectionChanged();
+            }
+        }
+    }
+
+    public class Subscriber
+    {
+        // Method to be executed when the event is raised
+        public async Task HandleEventAsync()
+        {
+            Console.WriteLine("Event handled asynchronously");
+            await Task.Delay(2000); // Simulating some asynchronous operation
+        }
+    }
+}
---
+++ FunctionBasedEventExample/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FunctionBasedEventExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        EventHandler eventHandler = new EventHandler();
+        Subscriber subscriber = new Subscriber();
+        public MainWindow()
+        {
+            InitializeComponent();
+            eventHandler.FunctionEvent += EventHandler_FunctionEvent;
+        }
+
+        private async Task EventHandler_FunctionEvent()
+        {
+            // Perform the necessary logic when the event is raised
+            // For example, updating UI elements, fetching data asynchronously, etc.
+            await Task.Delay(1000); // Simulate some asynchronous operation
+            MessageBox.Show("collection changed event fired!");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            eventHandler.TriggerCollectionChanged();
+        }
+    }
+}
